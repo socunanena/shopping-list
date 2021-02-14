0fe6dd05e54c08ac805d835eef4f55e7333ef21a
Natàlia <natalia.cavi@gmail.com>
2021-02-14 15:27:05 +0100

    Retrieved categories from DB

--- api/routes/categories.js
+++ api/routes/categories.js
@@ -1,29 +1,25 @@
 import { Router } from 'express';
-
-// To be emoved when implemented the real DB
-const categories = [
-  {
-    id: 0,
-    name: 'Carnisseria',
-  },
-  {
-    id: 1,
-    name: 'Fruiteria',
-  },
-  {
-    id: 2,
-    name: 'Peixateria',
-  },
-  {
-    id: 3,
-    name: 'Xarcuteria',
-  },
-];
+import mysql from 'mysql';
 
 const router = Router();
 
 router.get('/categories', (req, res) => {
-  res.json(categories);
+  const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'shopping-list',
+  });
+
+  connection.connect();
+
+  connection.query('SELECT * FROM categories', function (error, results, fields) {
+    if (error) console.log('ERROR', error);
+
+    res.json(results);
+  });
+
+  connection.end();
 });
 
 export default router;
