dd89c51cac288b1116a11b07ba8ea50da37a4ded
Natàlia <natalia.cavi@gmail.com>
2021-11-24 11:45:37 +0100

    Fixed theme error

--- vuetify.options.js
+++ vuetify.options.js
@@ -1,10 +1,12 @@
 import colors from 'vuetify/es5/util/colors';
 
+const { base, lighten5, lighten4, lighten3, lighten2, lighten1, darken1, darken2, darken3, darken4 } = colors.lightGreen;
+
 export default {
   theme: {
     themes: {
       light: {
-        primary: colors.lightGreen,
+        primary: { base, lighten5, lighten4, lighten3, lighten2, lighten1, darken1, darken2, darken3, darken4 },
       },
     },
   },
