fb3a310c45ee9fc24b1d125eb1e5ae5639b58697
Natàlia <natalia.cavi@gmail.com>
2021-11-24 12:48:30 +0100

    Renamed component to fit with Vue best practices

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app>
-    <Header />
+    <TheHeader />
 
     <v-main class="primary lighten-5">
       <v-container>
@@ -9,11 +9,3 @@
     </v-main>
   </v-app>
 </template>
-
-<script>
-import Header from '../components/Header.vue';
-
-export default {
-  components: { Header },
-};
-</script>
