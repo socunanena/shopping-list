78360efe765feff47d5a8dc9f83059df46089948
Natàlia <natalia.cavi@gmail.com>
2021-11-25 19:19:10 +0100

    Extended shopping list page to be able to check products

--- pages/index.vue
+++ pages/index.vue
@@ -24,26 +24,72 @@
       </v-col>
     </v-row>
 
-    <v-data-table
-      :headers="[{ value: 'name', filterable: false }, { value: 'list' }]"
-      :items="productsWithLists"
-      :items-per-page="-1"
-      class="elevation-1"
-      hide-default-header
-      hide-default-footer
-      item-key="name + list"
-      mobile-breakpoint=200
-      group-by="category"
-      :search="list.name"
-    >
-      <template v-slot:item.list>
-      </template>
-      <template v-slot:group.header="{ group, headers, toggle }">
-        <td :colspan="headers.length" @click="toggle">
-          <v-btn text plain elevation="0" class="text-subtitle-2 font-weight-bold">{{ group }}</v-btn>
-        </td>
-      </template>
-    </v-data-table>
+    <v-row>
+      <v-col>
+        <v-data-table
+          :headers="[{ value: 'name', filterable: false }, { value: 'isChecked', filter: value => !value }, { value: 'list' }]"
+          :items="productsWithLists"
+          :items-per-page="-1"
+          class="elevation-1"
+          hide-default-header
+          hide-default-footer
+          mobile-breakpoint=200
+          group-by="category"
+          :search="list.name"
+          show-select
+          v-model="checkedProducts"
+          @item-selected="checkProduct"
+        >
+          <template v-slot:item.isChecked>
+          </template>
+          <template v-slot:item.list>
+          </template>
+          <template v-slot:group.header="{ group, headers, toggle }">
+            <td :colspan="headers.length" @click="toggle">
+              <v-btn text plain elevation="0" class="text-subtitle-2 font-weight-bold">{{ group }}</v-btn>
+            </td>
+          </template>
+        </v-data-table>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col>
+        <v-expansion-panels flat>
+          <v-expansion-panel>
+            <v-expansion-panel-header color="primary lighten-5">
+              Checked products
+            </v-expansion-panel-header>
+            <v-expansion-panel-content color="primary lighten-5">
+              <v-data-table
+                :headers="[{ value: 'name', filterable: false }, { value: 'isChecked', filter: value => value }, { value: 'list' }]"
+                :items="productsWithLists"
+                :items-per-page="-1"
+                class="elevation-1"
+                hide-default-header
+                hide-default-footer
+                mobile-breakpoint=200
+                group-by="category"
+                :search="list.name"
+                show-select
+                v-model="checkedProducts"
+                @item-selected="checkProduct"
+              >
+                <template v-slot:item.isChecked>
+                </template>
+                <template v-slot:item.list>
+                </template>
+                <template v-slot:group.header="{ group, headers, toggle }">
+                  <td :colspan="headers.length" @click="toggle">
+                    <v-btn text plain elevation="0" class="text-subtitle-2 font-weight-bold">{{ group }}</v-btn>
+                  </td>
+                </template>
+              </v-data-table>
+            </v-expansion-panel-content>
+          </v-expansion-panel>
+        </v-expansion-panels>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
@@ -53,6 +99,7 @@ export default {
     return {
       list: {},
       product: {},
+      checkedProducts: [],
     };
   },
   methods: {
@@ -63,7 +110,11 @@ export default {
       }
 
       this.product = {};
-    }
+    },
+    checkProduct(product) {
+      this.productsWithLists.filter(p => p.id === product.item.id).forEach(p => p.isChecked = product.value);
+      this.$axios.$patch(`/products/check/${product.item.id}`, { isChecked: product.value });
+    },
   },
   async asyncData({ $axios }) {
     return {
@@ -74,6 +125,7 @@ export default {
   },
   created() {
     this.list = this.lists[0];
+    this.checkedProducts = this.productsWithLists.filter(p => p.isChecked);
   }
 };
 </script>
